896dd8169a5647af55ae861de9076c82185d585c
sanmiguel <michael.coles@gmail.com>
2017-02-22 18:27:16 +0100

    mix new sentinel --sup --app sentinel_core

---
+++ lib/sentinel_core.ex
@@ -0,0 +1,18 @@
+defmodule SentinelCore do
+  @moduledoc """
+  Documentation for SentinelCore.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SentinelCore.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
