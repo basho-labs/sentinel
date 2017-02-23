7ded0b4c662c134d22f85cbad33defaba2724da9
sanmiguel <michael.coles@gmail.com>
2017-02-23 16:45:45 +0100

    Use a MapSet for peers

--- lib/sentinel_router.ex
+++ lib/sentinel_router.ex
@@ -11,26 +11,26 @@ defmodule SentinelRouter do
     @doc """
     peers: a list of hostnames of machines in our immediate vicinity.
     """
-    defstruct peers: []
+    defstruct :peers
 
     def new(peer_hosts \\ []) do
-      %Network{peers: peer_hosts}
+      %Network{ peers: MapSet.new(peer_hosts) }
     end
     
     # TODO: Use a set or something, prevent dupes
     def peers(%Network{peers: prs}) do
-      prs
+      MapSet.to_list(prs)
     end
 
     def num_peers(%Network{peers: prs}) do
-      length(prs)
+      MapSet.size(prs)
     end
 
     @doc """
     Add a new peer to our known peers.
     """
     def add(%Network{peers: prs}, peer) do
-      %Network{peers: [ peer | prs ]}
+      %Network{peers: MapSet.put(prs, peer)}
     end
   end
 end
