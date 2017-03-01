6c4acb5e969312489364f0a097a0a185250b608a
Korry Clark <korriganclark@gmail.com>
2017-02-28 18:41:38 -0800

    update

--- relay/mqtt-message-relay.py
+++ relay/mqtt-message-relay.py
@@ -21,6 +21,7 @@ def on_log(_mqttc, obj, level, string):
     return
 
 def on_message(_mqttc, obj, msg):
+    topic = ''
     url = msg.topic.split('/')
     print('URL:')
     print(url)
@@ -29,32 +30,54 @@ def on_message(_mqttc, obj, msg):
     event = url[6]
     msg_format = url[8]
 
-    if deviceId not in gateways:
-        gateways.append(deviceId)
-
     #Support ping event and events identified by intended recipient
-    if event == 'ping':
+    if typeId == 'MyGateway':
 
-        if deviceId == 'test-gw-0':
-            sendDeviceId = 'test-gw-1'
-        if deviceId == 'test-gw-1':
-            sendDeviceId = 'test-gw-0'
-        if deviceId == 'test-gw-2':
-            sendDeviceId = 'test-gw-3'
-        if deviceId == 'test-gw-3':
-            sendDeviceId = 'test-gw-2'
-        if deviceId == 'test-gw-4':
-            sendDeviceId = 'test-gw-5'
-        if deviceId == 'test-gw-5':
-            sendDeviceId = 'test-gw-04'
-    else:
-        sendDeviceId = event
+        if event == 'ping':
 
-    relay_topic = 'iot-2/type/'+typeId+'/id/'+sendDeviceId+'/cmd/message/fmt/'+msg_format
-    send_url = relay_topic .split('/')
+            if deviceId == 'test-gw-0':
+                sendDeviceId = 'test-gw-1'
+            elif deviceId == 'test-gw-1':
+                sendDeviceId = 'test-gw-0'
+            elif deviceId == 'test-gw-2':
+                sendDeviceId = 'test-gw-3'
+            elif deviceId == 'test-gw-3':
+                sendDeviceId = 'test-gw-2'
+            elif deviceId == 'test-gw-4':
+                sendDeviceId = 'test-gw-5'
+            else:
+                sendDeviceId = 'test-gw-04'
+
+            topic = 'iot-2/type/'+typeId+'/id/'+sendDeviceId+'/cmd/message/fmt/'+msg_format
+            _mqttc.publish(topic, msg.payload)
+
+        else:
+            topic = 'iot-2/type/'+typeId+'/id/'+event+'/cmd/message/fmt/'+msg_format
+            _mqttc.publish(topic, msg.payload)
+
+    elif typeId == 'test-gateway':
+
+        if event == 'ping':
+            if deviceId not in gateways:
+                gateways.append(deviceId)
+            for gw in gateways:
+                if gw != deviceId:
+                    topic = 'iot-2/type/'+typeId+'/id/'+gw+'/cmd/'+event+'/fmt/'+msg_format
+                    _mqttc.publish(topic, msg.payload)
+
+        elif event != 'ping' and event in gateways:
+            topic = 'iot-2/type/'+typeId+'/id/'+event+'/cmd/message/fmt/'+msg_format
+            _mqttc.publish(topic, msg.payload)
+
+        elif event != 'ping' and event not in gateways and deviceId in gateways:
+            for gw in gateways:
+                if gw != deviceId:
+                    topic = 'iot-2/type/'+typeId+'/id/'+gw+'/cmd/'+event+'/fmt/'+msg_format
+                    _mqttc.publish(topic, msg.payload)
+
+    send_url = topic.split('/')
     print('Send URL:')
     print(send_url)
-    _mqttc.publish(relay_topic, msg.payload)
     return
 
 mqttc = mqtt.Client('a:4tlin1:a2g6k39sl6r5')
