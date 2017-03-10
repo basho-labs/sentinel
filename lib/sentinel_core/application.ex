82a4bc78421a2bffa8c77c995b7b599f25f7462c
sanmiguel <michael.coles@gmail.com>
2017-03-10 23:57:16 +0100

    Turns out the ':name' option does nothing.

--- lib/sentinel_core/application.ex
+++ lib/sentinel_core/application.ex
@@ -11,8 +11,8 @@ defmodule SentinelCore.Application do
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: SentinelCore.Worker.start_link(arg1, arg2, arg3)
-      supervisor(SentinelCore.PeerSupervisor, [], name: SentinelCore.PeerSupervisor),
-      worker(SentinelCore.Switchboard, [], name: SentinelCore.Switchboard)
+      supervisor(SentinelCore.PeerSupervisor, []),
+      worker(SentinelCore.Switchboard, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
